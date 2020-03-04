fbc7a4fac1d072f6cb8a0ad792ef50ea0254431e
Ivana Maldonado <imaldona19@gmail.com>
2020-03-04 10:46:13 +0000

    dashboard layout

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,14 +1,27 @@
+require 'byebug'
+require 'date'
 
 class ContentsController < ApplicationController
 
   def index
-    @service =FetchTitles.new
+    @service =FetchTitlesJson.new
 
-    @countries = @service.getCountries
+    # @countries = @service.getCountries
     country = current_user.country
 
     @content = @service.getExpiringContent(country)
 
+    groupByWeek
+  end
+
+  def groupByWeek
+   @weeks = @content.group_by { |item| item[:expiration_date].cweek }
+   @weeks.keys
+
+   @week33 = @weeks[33]
+   @week34 = @weeks[34]
+   @week35 = @weeks[35]
+   @week36 = @weeks[36]
   end
 
   # def show
