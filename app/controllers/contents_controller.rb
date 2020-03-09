5e1173ab0b9d3db9b9213f01afc2f7ef3858f1b9
Ivana Maldonado <imaldona19@gmail.com>
2020-03-09 09:48:53 +0000

    more filters

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,24 +1,34 @@
 require 'date'
 
 class ContentsController < ApplicationController
-
-  @category_filter = nil
-  @rating_filter = nil
-
+  
   def index
     @service = FetchTitlesService.new
     @service.get_expiring_content(current_user.country)
     @contents = Content.where(country_code: current_user.country)
 
-    @contents = @contents.where(category: 'movie') if params[:category] == 'movie'
-    @contents = @contents.where(category: 'series') if params[:category] == 'series'
-
+    filter_content
+    
     @weeks = @contents.group_by { |content| content.expiration_date.cweek }.sort
 
   end
+  
+  def filter_content 
+    if params[:category]
+      @contents = @contents.where(category: params[:category])
+    end
+    
+    if params[:imdb_rating]
+      @contents = @contents.select { |content| content[:imdb_rating] > params[:imdb_rating].to_f }
+    end
 
+    if params[:genre]
+      @contents = @contents.select { |content| content[:genre].include? params[:genre] }
+    end 
+  end 
+  
   def show
     @content = Content.find(params[:id])
   end
-
+  
 end
