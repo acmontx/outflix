7748f4436589d5d7d23353c0e870d10a3190c522
Vanessa Santo <vanessaesanto@gmail.com>
2020-03-11 14:54:04 +0000

    start rake task

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,8 +8,8 @@ class NotificationsController < ApplicationController
     # current_user[:notification_size] = params[:notifications][:notification_size]
     #current_user.save!
    if current_user.save
-    UserMailer.with(user: current_user).newsletter.deliver_now if current_user[:frequency] == "Demo - deliver now"
-    redirect_to contents_path
+      UserMailer.with(user: current_user).newsletter.deliver_now if current_user[:frequency] == "Demo - deliver now"
+      redirect_to contents_path
     else
       render :index
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,23 +5,21 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.expiring.subject
   #
-  def welcome_email
+  def expiring
     @user = params[:user] # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Welcome to Outflix!')
+    mail(to: @user.email, subject: 'Expiring Netf..x Titles!')
     # This will render a view in `app/views/user_mailer`!
   end
 
-  def newsletter
+  def dashboard
     @user = params[:user]
     @service = FetchTitlesService.new
     @service.get_expiring_content(@user.country)
     @contents = Content.where(country_code: @user.country)
+
     @weeks = @contents.group_by { |content| content.expiration_date.cweek }.sort
 
-
-    mail(to: @user.email, subject: 'Outflix newsletter!')
+    mail(to: @user.email, subject: 'Dashboard')
   end
-
-
 end
